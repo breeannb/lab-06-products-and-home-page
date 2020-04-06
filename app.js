2866e3b4fd8a91c9693d491a76b4562c51595870
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-04-06 16:56:45 -0700

    completed step 4

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Importing my variables and array of my bugs
+// Importing my variables and array of my bugs and render function 
 import bugs from './data.js'; 
 import renderBug from './render-products.js'; 
 
@@ -7,9 +7,11 @@ const bugContainer = document.getElementById('container');
 
 for (let i = 0; i < bugs.length; i++) { // 
     const bug = bugs[i]; // to get the bugs from the array 
-    const li = renderBug(bug);
+    const li = renderBug(bug); // Pass to your DOM generation function and capture result in variable
 
+// Append to the top-level list element
     bugContainer.appendChild(li); 
 
 }
+
 
