851d0d86a907af6ce7ccdb1d3d1e1fb3bb285301
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-04-08 22:38:01 -0700

    completed step 1, adding product to shopping cart

--- render-products.js
+++ render-products.js
@@ -1,4 +1,7 @@
+import findByID from './common/utils.js';
+
 export default function renderBug(bug) { 
+
     // make a div 
     const li = document.createElement('li');
     const h1 = document.createElement('h1');
@@ -12,8 +15,39 @@ export default function renderBug(bug) {
     img.src = bug.image; 
     description.textContent = 'Description: ' + bug.description; 
     price.textContent = 'Price: $' + bug.price;
-    button.innerHTML = 'Add'; 
+    button.textContent = 'Add';
+    button.value = bug.id;  
+    
+    // button Event Listener
     button.addEventListener('click', function(){
+        // retrieve existing shopping cart from localStorage. If no cart, we need an empty array. If there is, we need to turn it into a json.parse
+        let cartInLocalStorage = localStorage.getItem('CART'); // set variable to cart in local storage
+        let cart; //creating javascript cart 
+
+        //if there is a cart: turn a string cart into an object
+        if (cartInLocalStorage) { 
+            cart = JSON.parse(cartInLocalStorage);
+        }
+        else { //if no cart, go make an empty cart 
+            cart = [];
+        }
+
+        let cartItem = findByID(cart, bug.id); 
+
+        if (!cartItem) {
+            const cartItem = {
+                id: bug.id,
+                quantity: 1
+            };
+
+            cart.push(cartItem); 
+        } else 
+            cartItem.quantity++; 
+
+        //stringify our shopping cart 
+        const stringCart = JSON.stringify(cart);
+        // put string cart in local storage 
+        localStorage.setItem('CART', stringCart); 
 
     });
 
