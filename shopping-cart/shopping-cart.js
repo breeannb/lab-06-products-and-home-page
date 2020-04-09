f42db4f092a4f362409515382e0f84c7e7612cf3
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-04-09 14:25:17 -0700

    worked with Jacob to solve my const issue so now my cart populates the cart items added

--- shopping-cart/shopping-cart.js
+++ shopping-cart/shopping-cart.js
@@ -2,43 +2,46 @@
 import { renderTableRow } from './render-line-items.js'; 
 import bugs from '../data.js'; 
 // import makeReadablePrice from '../shopping-cart/render-line-items.js';
-import cart from '../data/cart.js'; 
+// import cart from '../data/cart.js'; 
 import findByID, { calcOrderTotal, toUSD } from '../common/utils.js';
 
 const tbody = document.getElementById('tbody');
 const orderTotalCell = document.getElementById('order-total-cell');
 
+// Get shopping cart from local storage to the Shopping Cart webpage 
+const cartInLocalStorage = localStorage.getItem('CART'); // getItem from local Storage, the stringyCart, and set to cartInLocalStorage
+let cart;
 
-const cartInLocalStorage = localStorage.getItem('CART'); 
-const cart; 
-// if there is a cart in local storage 
-if (cartInLocalStorage) { 
-    cart = JSON.parse(cartInLocalStorage);
+if (cartInLocalStorage) { // if the cart in local storage exists
+    cart = JSON.parse(cartInLocalStorage); //parse(unstringify) the cart 
 }
 else {
-    cart = [];
+    cart = []; //initalize the cart
 }
 
+
+//looping
 for (let i = 0; i < cart.length; i++) { 
     const cartItem = cart[i]; 
-    const constItemId = cartItem.id;
+    const constItemId = cartItem.id; //finding the cartItem ID that corresonds to that ID
     const bug = findByID(bugs, constItemId); 
-    const dom = renderTableRow(cartItem, bug); 
-    tbody.appendChild(dom); 
+    const dom = renderTableRow(cartItem, bug); //renders new table row 
+    tbody.appendChild(dom); //append to tbody 
 }
 const orderTotal = calcOrderTotal(cart, bugs); 
 orderTotalCell.textContent = toUSD(orderTotal);
 
 
-// if the cart is empty 
-if (cart.length === 0)  {
-    placeOrderButton.disabled = true; 
-}
-else { 
-    //if there is something in the cart, add event listener to order button
-    placeOrderButton.addEventListener('click', () =>{
-        localStorage.removeItem('CART'); 
-        alert('Order placed:\n' + JSON.stringify(cart, true, 2));
-        window.location = '../'; 
-    });
-}
+// // if the cart is empty 
+// if (cart.length === 0)  {
+//     placeOrderButton.disabled = true; 
+// }
+// else { 
+//     //if there is something in the cart, add event listener to order button
+//     placeOrderButton.addEventListener('click', () =>{
+//         localStorage.removeItem('CART'); 
+//         alert('Order placed:\n' + JSON.stringify(cart, true, 2));
+//         window.location = '../'; 
+//     });
+
+// }
